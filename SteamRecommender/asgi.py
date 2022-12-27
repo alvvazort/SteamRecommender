9a152f6e78e9b0ef0694425f472f159300092638
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-12-27 14:11:05 +0100

    Refactor rename

--- SteamRecommender/asgi.py
+++ SteamRecommender/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EjercicioRS project.
+ASGI config for SteamRecommender project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EjercicioRS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SteamRecommender.settings')
 
 application = get_asgi_application()
--- SteamRecommender/wsgi.py
+++ SteamRecommender/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EjercicioRS project.
+WSGI config for SteamRecommender project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EjercicioRS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SteamRecommender.settings')
 
 application = get_wsgi_application()
